53e09e6f236592142d68179c1f28a75f531a4a0d
Emily Chai <echai93@gmail.com>
2017-05-15 11:35:50 +0100

    Deposit and withdraw functionality to change account balance

--- lib/account.rb
+++ lib/account.rb
@@ -6,6 +6,14 @@ class Account
     @balance = 100
   end
 
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def withdraw(amount)
+    self.balance -= amount
+  end
+  
   private
   attr_writer :balance
 
