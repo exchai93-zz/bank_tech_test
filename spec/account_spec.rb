931e55b0bfcf11ebf640a2cc76c0cb60951da904
Emily Chai <echai93@gmail.com>
2017-05-15 14:38:21 +0100

    WIP withdrawing throw error when funds are below 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,22 +2,38 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
+  let(:statement) { Statement.new }
 
-  describe '#initialize' do
-    it 'has an initial balance of 0' do
-      expect(account.balance).to eq 0
+  context 'New account' do
+    describe '#initialize' do
+      it 'has a balance of 0' do
+        expect(account.balance).to eq 0
+      end
     end
   end
 
-  describe '#deposit' do
-    it 'adds to the account balance' do
-      expect{ account.deposit(25) }.to change{ account.balance }.by 25
+  context 'Making transactions' do
+    describe '#deposit' do
+      it 'adds to the account balance' do
+        expect{ account.deposit(25) }.to change{ account.balance }.by 25
+      end
+    end
+
+    describe '#withdraw' do
+      it 'deducts from the account balance' do
+        expect{ account.withdraw(50) }.to change{ account.balance }.by -50
+      end
+
+      it 'raises an error if funds are below zero' do
+        account.deposit(5)
+        expect{ account.withdraw(20) }.to raise_error 'Insufficient funds'
+      end
     end
   end
 
-  describe '#withdraw' do
-    it 'deducts from the account balance' do
-      expect{ account.withdraw(50) }.to change{ account.balance }.by -50
+  describe '#statement' do
+    it 'displays statement in the correct format' do
+      expect(account.statement). to eq "date || credit || debit || balance"
     end
   end
 end
